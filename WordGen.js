c3385e2d76111d5a46e442ef2657da7d9ddaa85a
nicfv <omg.my.junkmail@gmail.com>
2022-03-29 13:15:48 -0700

    Display valid words, need to filter for incorrect placement letters

---
+++ WordGen.js
@@ -0,0 +1,30 @@
+'use strict';
+
+class WordGen {
+    #charManager;
+    constructor(charManager) {
+        this.#charManager = charManager;
+    }
+
+    getList() {
+        let i = 0,
+            c1 = this.#charManager.generateValidCharsForPosition(0),
+            c2 = this.#charManager.generateValidCharsForPosition(1),
+            c3 = this.#charManager.generateValidCharsForPosition(2),
+            c4 = this.#charManager.generateValidCharsForPosition(3),
+            c5 = this.#charManager.generateValidCharsForPosition(4),
+            wordList = [];
+        for (let i1 in c1) {
+            for (let i2 in c2) {
+                for (let i3 in c3) {
+                    for (let i4 in c4) {
+                        for (let i5 in c5) {
+                            wordList[i++] = c1[i1] + c2[i2] + c3[i3] + c4[i4] + c5[i5];
+                        }
+                    }
+                }
+            }
+        }
+        return wordList;
+    }
+}
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 'use strict';
 
-let CM;
+let CM, WL;
 
 window.onload = () => {
     CM = new CharManager('abcde', [1, 2, 3, 4, 5]);
+    WL = new WordGen(CM);
+
+    const genButton = document.getElementById('gen'),
+        words = document.getElementById('words');
+
+    genButton.onclick = () => {
+        const list = WL.getList();
+        words.innerText = '';
+        for (let i in list) {
+            if (i > 1e3) {
+                break;
+            }
+            words.innerText += list[i] + '\n';
+        }
+    };
 };
