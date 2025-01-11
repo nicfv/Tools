88e771f578aaa9e1139f0861c20234ca0a273539
Nic <github@nicfv.com>
2025-01-11 12:11:07 -0800

    Add code for dropdowns

--- www/interest/src/index.ts
+++ www/interest/src/index.ts
@@ -1,5 +1,6 @@
 import { Input } from './input';
 import { Button } from './button';
+import { Dropdown } from './dropdown';
 
 window.addEventListener('load', main);
 
@@ -8,4 +9,6 @@ function main(): void {
     const b = new Input(document.body, 'test', -100, 100, 0.1);
     const c = new Button(document.body, 'Click!');
     c.onClick(() => b.clear());
+    const d = new Dropdown(document.body, 'test1', [{ key: 'hello', value: 'one' }, { key: 'world', value: 'two' }]);
+    d.onChange(v => console.log(v));
 }
--- www/interest/src/input.ts
+++ www/interest/src/input.ts
@@ -4,26 +4,27 @@ import { SMath } from 'smath';
  * Represents a numeric input element with a label.
  */
 export class Input {
-    private static input_id: number = 0;
+    private static readonly className = 'input';
+    private static id: number = 0;
     private readonly element: HTMLInputElement;
     /**
      * Create a new input element and append onto the parent element.
      */
     constructor(parent: Element, title: string, private readonly min: number, private readonly max: number, step: number) {
-        Input.input_id++;
+        Input.id++;
         this.element = document.createElement('input');
         const container = document.createElement('div'),
             label = document.createElement('label'),
-            inputId = 'input_' + Input.input_id;
-        container.setAttribute('class', 'input');
-        this.element.setAttribute('id', 'input_' + inputId);
+            inputId = Input.className + '_' + Input.id;
+        container.setAttribute('class', Input.className);
+        this.element.setAttribute('id', inputId);
         this.element.setAttribute('type', 'number');
         this.element.setAttribute('title', title);
         this.element.setAttribute('placeholder', title);
         this.element.setAttribute('min', min.toString());
         this.element.setAttribute('max', max.toString());
         this.element.setAttribute('step', step.toString());
-        label.setAttribute('for', 'input_' + inputId);
+        label.setAttribute('for', inputId);
         label.innerText = title;
         container.appendChild(this.element);
         container.appendChild(label);
