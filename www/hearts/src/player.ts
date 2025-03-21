ef39625045f423229792216e4313e438a367548e
nicfv <github@nicfv.com>
2025-03-21 15:42:03 -0700

    Player improvements

--- www/hearts/src/player.ts
+++ www/hearts/src/player.ts
@@ -1,14 +1,39 @@
 import { Card } from './card';
+import { Deck } from './deck';
 
 export class Player {
     public score: number = 0;
     public readonly missingSuits: Array<number> = [];
+    constructor(private readonly passTarget: boolean) { }
     public play(card: Card, leadSuit: number): void {
+        card.played = true;
         if (card.suit !== leadSuit) {
             this.missingSuits.push(leadSuit);
         }
     }
     public take(trick: Array<Card>): void {
         trick.forEach(card => this.score += card.pointValue);
+    }
+    public possibleCards(deck: Deck): Array<Card> {
+        return deck.cards.filter(card => {
+            // Card was already played.
+            if (card.played) {
+                return false;
+            }
+            // Card is in your hand.
+            if (card.inHand) {
+                return false;
+            }
+            // Card was passed to someone else.
+            if (card.passed && !this.passTarget) {
+                return false;
+            }
+            // Out of this suit.
+            if (this.missingSuits.includes(card.suit)) {
+                return false;
+            }
+            // Might have this card.
+            return true;
+        });
     }
 }
