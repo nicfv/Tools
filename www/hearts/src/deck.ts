ad6887847c86b17e7ecba12fd4bc0dfefc65ca90
nicfv <github@nicfv.com>
2025-03-21 15:44:59 -0700

    Add comments

--- www/hearts/src/deck.ts
+++ www/hearts/src/deck.ts
@@ -1,7 +1,13 @@
 import { Card } from './card';
 
+/**
+ * Represents a standard deck of 52 cards.
+ */
 export class Deck {
     public readonly cards: Array<Card>;
+    /**
+     * Re-deal the deck.
+     */
     constructor() {
         this.cards = [];
         for (let suit = 1; suit <= 4; suit++) {
--- www/hearts/src/player.ts
+++ www/hearts/src/player.ts
@@ -1,19 +1,41 @@
 import { Card } from './card';
 import { Deck } from './deck';
 
+/**
+ * Represents a player in the game.
+ */
 export class Player {
+    /**
+     * The player's score for the current round.
+     */
     public score: number = 0;
+    /**
+     * Suits that this player doesn't have.
+     */
     public readonly missingSuits: Array<number> = [];
+    /**
+     * Create a new player.
+     * @param passTarget Whether or not this player is the target of card passing.
+     */
     constructor(private readonly passTarget: boolean) { }
+    /**
+     * This player plays a card.
+     */
     public play(card: Card, leadSuit: number): void {
         card.played = true;
         if (card.suit !== leadSuit) {
             this.missingSuits.push(leadSuit);
         }
     }
+    /**
+     * This player takes the trick.
+     */
     public take(trick: Array<Card>): void {
         trick.forEach(card => this.score += card.pointValue);
     }
+    /**
+     * Get a list of all possible cards in this player's hand.
+     */
     public possibleCards(deck: Deck): Array<Card> {
         return deck.cards.filter(card => {
             // Card was already played.
