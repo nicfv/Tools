e46342cfd87d9888d3de5499f3820ba677803e7a
nicfv <github@nicfv.com>
2025-09-04 17:29:34 -0700

    Add docs

--- www/dimensional/src/types.ts
+++ www/dimensional/src/types.ts
@@ -1,11 +1,29 @@
 import { Unit } from 'dimensional';
 
+/**
+ * Represents a unit with a name
+ */
 export interface NamedUnit {
+    /**
+     * The name of this unit
+     */
     readonly name: string;
+    /**
+     * The unit represented by the name
+     */
     readonly unit: Unit;
 }
 
+/**
+ * Represents an input/output pair of objects
+ */
 export interface Pair<T> {
+    /**
+     * The input object
+     */
     input: T;
+    /**
+     * The output object
+     */
     output: T;
 }
