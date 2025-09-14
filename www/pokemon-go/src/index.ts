01804b7127804dec538bca920bded1bce641c2f5
Nic <github@nicfv.com>
2025-09-14 15:58:22 -0700

    defer script

--- www/pokemon-go/src/index.ts
+++ www/pokemon-go/src/index.ts
@@ -1,39 +1,6 @@
 import { Dropdown, DropdownItem } from './dropdown';
 import { Range } from './range';
 
-window.addEventListener('load', main);
-
-function main(): void {
-    console.log('Loaded!');
-    const form = document.getElementById('form')!,
-        output = document.getElementById('output')!,
-        BCR = new Range(form, 'Base Capture Rate [0-100%]', true, { min: 0, max: 100, step: 0.5 }),
-        CPM = new Range(form, 'CP Multiplier [0.094-0.8653]', true, { min: 0.094, max: 0.8653, step: 0.0001 }),
-        ball = new Dropdown(form, 'Ball Type', BallType),
-        berry = new Dropdown(form, 'Berry Type', BerryType),
-        throwtype = new Dropdown(form, 'Throw Modifier', ThrowType),
-        curve = new Dropdown(form, 'Throw Style', CurveType),
-        medal = new Dropdown(form, 'Type-Specific Medal', MedalType),
-        encounter = new Dropdown(form, 'Encounter Type', EncounterType),
-        rate = new Range(output, 'Catch Rate [0-100%]', false, { min: 0, max: 100, step: 0.125 });
-    BCR.setValue(20);
-    CPM.setValue(0.64);
-    BCR.onChange(calculate);
-    CPM.onChange(calculate);
-    ball.onChange(calculate);
-    berry.onChange(calculate);
-    throwtype.onChange(calculate);
-    curve.onChange(calculate);
-    medal.onChange(calculate);
-    encounter.onChange(calculate);
-    function calculate(): void {
-        console.log('Calculating...');
-        const modifier: number = ball.getValue() * berry.getValue() * throwtype.getValue() * curve.getValue() * medal.getValue() * encounter.getValue();
-        rate.setValue(100 - 100 * (1 - BCR.getValue() / 100 / 2 / CPM.getValue()) ** modifier);
-    }
-    calculate();
-}
-
 const BallType: Array<DropdownItem> = [
     { key: 'Poke Ball', value: 1 },
     { key: 'Great Ball', value: 1.5 },
@@ -72,3 +39,32 @@ const EncounterType: Array<DropdownItem> = [
     { key: 'Wild Encounter', value: 1 },
     { key: 'Research Reward', value: 2 },
 ];
+
+console.log('Loaded!');
+const form = document.getElementById('form')!,
+    output = document.getElementById('output')!,
+    BCR = new Range(form, 'Base Capture Rate [0-100%]', true, { min: 0, max: 100, step: 0.5 }),
+    CPM = new Range(form, 'CP Multiplier [0.094-0.8653]', true, { min: 0.094, max: 0.8653, step: 0.0001 }),
+    ball = new Dropdown(form, 'Ball Type', BallType),
+    berry = new Dropdown(form, 'Berry Type', BerryType),
+    throwtype = new Dropdown(form, 'Throw Modifier', ThrowType),
+    curve = new Dropdown(form, 'Throw Style', CurveType),
+    medal = new Dropdown(form, 'Type-Specific Medal', MedalType),
+    encounter = new Dropdown(form, 'Encounter Type', EncounterType),
+    rate = new Range(output, 'Catch Rate [0-100%]', false, { min: 0, max: 100, step: 0.125 });
+BCR.setValue(20);
+CPM.setValue(0.64);
+BCR.onChange(calculate);
+CPM.onChange(calculate);
+ball.onChange(calculate);
+berry.onChange(calculate);
+throwtype.onChange(calculate);
+curve.onChange(calculate);
+medal.onChange(calculate);
+encounter.onChange(calculate);
+function calculate(): void {
+    console.log('Calculating...');
+    const modifier: number = ball.getValue() * berry.getValue() * throwtype.getValue() * curve.getValue() * medal.getValue() * encounter.getValue();
+    rate.setValue(100 - 100 * (1 - BCR.getValue() / 100 / 2 / CPM.getValue()) ** modifier);
+}
+calculate();
