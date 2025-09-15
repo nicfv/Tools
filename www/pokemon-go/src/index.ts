c31333b0b3227fd323c767d21853dffcec00df00
Nic <github@nicfv.com>
2025-09-14 17:03:17 -0700

    test out ring

--- www/pokemon-go/src/index.ts
+++ www/pokemon-go/src/index.ts
@@ -1,4 +1,5 @@
 import { Range } from './range';
+import { Ring } from './ring';
 
 console.log('Loaded!');
 const form = document.getElementById('form')!;
@@ -6,11 +7,13 @@ const output = document.getElementById('output')!;
 const color = new Range(form, 'Ring Color', { min: 0, max: 100, step: 1, val: 50 });
 const size = new Range(form, 'Ring Size', { min: 0, max: 100, step: 1, val: 50 });
 const curve = new Range(form, 'Throw was a curveball?', { min: 1, max: 2, step: 1, val: 1 });
+const ring = new Ring(100, 5, document.body);
 color.onChange(calculate);
 size.onChange(calculate);
 curve.onChange(calculate);
 function calculate(): void {
-    console.log('Calculating...');
-    output.textContent = [color.getValue(), size.getValue(), curve.getValue()].join();
+    console.log(`Calculating with parameters: color=${color.getValue()}, size=${size.getValue()}, curve=${curve.getValue()}...`);
+    ring.setColor(color.getValue() / 100);
+    ring.setSize(size.getValue() / 100);
 }
 calculate();
