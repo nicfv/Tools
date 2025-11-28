6bc0a476c67ff3550be300abeca0f616b093d6dc
Nic <github@nicfv.com>
2025-11-28 15:02:36 -0800

    clean up script

--- www/data-url/src/index.ts
+++ www/data-url/src/index.ts
@@ -1,13 +1,13 @@
 const fileInput = document.getElementById('file_input') as HTMLInputElement;
 const outputText = document.getElementById('output') as HTMLTextAreaElement;
 
-fileInput.addEventListener('change', e => {
+fileInput.addEventListener('change', () => {
     const file: File | undefined = fileInput.files?.[0];
     const reader: FileReader = new FileReader();
     if (file) {
         reader.readAsDataURL(file);
     } else {
-        alert('File not found.');
+        outputText.textContent = 'File not found.';
     }
     reader.addEventListener('load', () => {
         outputText.textContent = (reader.result as string);
