5c098c63f68de52d40d85188a62cc2e2c2241377
Nic <github@nicfv.com>
2025-11-28 14:44:00 -0800

    add basic code

--- www/data-url/src/index.ts
+++ www/data-url/src/index.ts
@@ -1,3 +1,16 @@
-let me: string;
-me = 'world';
-console.log(`Hello, ${me}!`);
+const fileInput = document.getElementById('file_input') as HTMLInputElement;
+const convBtn = document.getElementById('convert') as HTMLButtonElement;
+const outputText = document.getElementById('output') as HTMLTextAreaElement;
+
+fileInput.addEventListener('change', e => {
+    const file: File | undefined = fileInput.files?.[0];
+    const reader: FileReader = new FileReader();
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        alert('File not found.');
+    }
+    reader.addEventListener('load', e => {
+        console.log(reader.result);
+    });
+});
